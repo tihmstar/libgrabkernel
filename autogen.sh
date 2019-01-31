ddfaac0da4f10a9a2f3fafda18d4c92d703d0593
tihmstar <tihmstar@gmail.com>
2019-01-31 22:24:47 +0100

    forgot autogen.sh lol

---
+++ autogen.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+gprefix=`which glibtoolize 2>&1 >/dev/null`
+if [ $? -eq 0 ]; then
+  glibtoolize --force
+else
+  libtoolize --force
+fi
+aclocal -I m4
+autoconf
+autoheader
+automake --add-missing
+
+
+SYSROOT="$(xcrun --show-sdk-path --sdk iphoneos)"
+export CFLAGS+="-arch arm64 -isysroot $SYSROOT -I$PWD/deps/curl-static/"
+export CXXFLAGS+="-arch arm64 -isysroot $SYSROOT -I$PWD/deps/curl-static/"
+export LDFLAGS+="-L$PWD/deps/curl-static"
+
+
+CONFIGURE_FLAGS="--enable-static --disable-shared\
+  --build=x86_64-apple-darwin`uname -r` \
+  --host=aarch64-apple-darwin \
+  $@"
+
+SUBDIRS="external/libfragmentzip"
+for SUB in $SUBDIRS; do
+    pushd $SUB
+    ./autogen.sh $CONFIGURE_FLAGS
+    popd
+done
+
+./configure $CONFIGURE_FLAGS
+
